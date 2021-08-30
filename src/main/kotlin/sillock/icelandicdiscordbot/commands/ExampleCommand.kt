0e84ffd2cabcf152015f6f84b8124d78819f53b1
jared <jprest97@gmail.com>
2021-08-30 14:43:49 +0100

    Fixed waiting bug and added parameter for the amount of examples

--- src/main/kotlin/sillock/icelandicdiscordbot/commands/ExampleCommand.kt
+++ src/main/kotlin/sillock/icelandicdiscordbot/commands/ExampleCommand.kt
@@ -7,6 +7,7 @@ import org.javacord.api.interaction.SlashCommandOptionType
 import org.openqa.selenium.By
 import org.openqa.selenium.Keys
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
@@ -19,28 +20,37 @@ class ExampleCommand: ICommand {
         get() = "Get usage of word"
     override val options: List<SlashCommandOption>
         get() = listOf(
-            SlashCommandOption.create(SlashCommandOptionType.STRING, "word", "Word to search by", true)
+            SlashCommandOption.create(SlashCommandOptionType.STRING, "word", "Word to search by", true),
+            SlashCommandOption.create(SlashCommandOptionType.INTEGER, "amount", "Amount of examples", false)
         )
 
 
     override fun execute(event: SlashCommandInteraction) {
-
         event.createImmediateResponder().setContent("Examples:").respond()
+        val exampleCount = if (!event.secondOptionIntValue.isEmpty) event.secondOptionIntValue.get() else 6
+        var messageBuilder = MessageBuilder()
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\jpres\\Downloads\\geckodriver-v0.29.1-win64\\geckodriver.exe")
         val driver = FirefoxDriver()
         val wait = WebDriverWait(driver, 10)
         driver.get("https://islenskordabok.arnastofnun.is")
         driver.findElement(By.name("searchBoxInput")).sendKeys(event.firstOptionStringValue.get() + Keys.ENTER)
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS)
-      //  wait.until(ExpectedConditions.presenceOfElementLocated(By.ByCssSelector("table[class*='list-group.mb-4.dictionary-list']")))
-        driver.findElement(By.ByCssSelector("a[href*='ord']")).click()
-        val firstResult = driver.findElement(By.ByCssSelector("div[class*='dict-item dict-section']"))
-        val examples = firstResult.findElements(By.ByCssSelector("div[class*='dict-item dict-daemi']"))
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.ByCssSelector("a[href*='ord']")))
+        val searchElement = driver.findElements(By.ByClassName("breadcrumb"))
+        if(searchElement.isNotEmpty()) {
+            val element = driver.findElement(By.ByCssSelector("a[href*='ord']"))
+            element?.click()
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.ByClassName("dict-view")))
+        }
 
-        //firstResult.findElements(By.tagName("tr")).first().findElements(By.tagName("td")).first().findElements(By.tagName("a href")).first()
-        var messageBuilder = MessageBuilder()
+        val examples = driver.findElements(By.ByCssSelector("div[class*='dict-item dict-daemi']"))
+
+        if(examples.isEmpty()){
+            messageBuilder.setContent("No examples available").send(event.channel.get())
+            return
+        }
+
         var result: String = ""
-        examples.forEach { x ->
+        examples.take(exampleCount).forEach { x ->
             result += x.text + "\n"
         }
         messageBuilder.setContent(result).send(event.channel.get())
